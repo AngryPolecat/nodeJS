839bd52908bd8f835c8641627e317e496d45ede6
polecat <mvv.tfoms@gmail.com>
2024-08-18 18:41:23 +0700

    update 04-web-server 18.08 18:41

--- 04-web-server/index2.js
+++ 04-web-server/index2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
-const { addNote, getNotes, removeNote } = require('./note.controller.js');
+const { addNote, getNotes, removeNote, updateNote } = require('./note.controller.js');
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', 'pages');
 
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(
   express.urlencoded({
@@ -25,16 +26,27 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-  await addNote(req.body.title);
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: true,
-  });
+  if (req.body.title) {
+    await addNote(req.body.title);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      created: true,
+    });
+  }
 });
 
 app.delete('/:id', async (req, res) => {
   await removeNote(req.params.id);
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false,
+  });
+});
+
+app.put('/:id', async (req, res) => {
+  await updateNote(req.params.id, req.body.title);
   res.render('index', {
     title: 'Express App',
     notes: await getNotes(),
--- 04-web-server/public/app.js
+++ 04-web-server/public/app.js
@@ -8,9 +8,28 @@ document.addEventListener('click', (event) => {
       event.target.closest('li').remove();
     });
   }
+
+  if (dataType === 'edit') {
+    const id = event.target.dataset.id;
+    const text = event.target.dataset.title;
+    const newText = prompt('Редактировать', text);
+    if (newText) {
+      updateNote(id, newText).then((res) => {
+        const containerTitle = event.target.closest('li').querySelector('.container-title');
+        containerTitle.innerHTML = newText;
+      });
+    }
+  }
 });
 
 const removeNote = async (id) =>
   await fetch(`/${id}`, {
     method: 'DELETE',
+  });
+
+const updateNote = async (id, title) =>
+  await fetch(`/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify({ title }),
   });
