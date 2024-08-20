0657f10915dcbecd8d673e1c4e27927868e428e7
AngryPolecat <mvv.tfoms@gmail.com>
2024-08-20 14:38:46 +0700

    done extra task 1 20.08 14:38

--- 02-basics/index.js
+++ 02-basics/index.js
@@ -1,11 +1,11 @@
-const yargs = require('yargs');
-const { printNotes, addNote, removeNote } = require('./note.controller');
+const yargs = require('yargs')
+const { printNotes, addNote, removeNote, editNote } = require('./note.controller')
 
 yargs.command({
   command: 'add',
   describe: 'Add new note to list',
   handler({ title }) {
-    addNote(title);
+    addNote(title)
   },
   builder: {
     title: {
@@ -14,21 +14,21 @@ yargs.command({
       demandOption: 'true',
     },
   },
-});
+})
 
 yargs.command({
   command: 'list',
   describe: 'Show all notes',
   async handler() {
-    printNotes();
+    printNotes()
   },
-});
+})
 
 yargs.command({
   command: 'remove',
   describe: 'Remove note by id',
   async handler({ id }) {
-    removeNote(id);
+    removeNote(id)
   },
   builder: {
     id: {
@@ -37,6 +37,26 @@ yargs.command({
       demandOption: 'true',
     },
   },
-});
+})
 
-yargs.parse();
+yargs.command({
+  command: 'edit',
+  describe: 'Edit note by id',
+  async handler({ id, title }) {
+    editNote(id, title)
+  },
+  builder: {
+    id: {
+      type: Number,
+      describe: 'Note id',
+      demandOption: 'true',
+    },
+    title: {
+      type: String,
+      describe: 'Note title',
+      demandOption: 'true',
+    },
+  },
+})
+
+yargs.parse()
--- 02-basics/note.controller.js
+++ 02-basics/note.controller.js
@@ -1,42 +1,55 @@
-const fs = require('fs/promises');
-const chalk = require('chalk');
-const path = require('path');
+const fs = require('fs/promises')
+const chalk = require('chalk')
+const path = require('path')
 
-const notesPath = path.join(__dirname, 'db.json');
+const notesPath = path.join(__dirname, 'db.json')
 
-const writeNotesToFile = async (notes) => await fs.writeFile(notesPath, JSON.stringify(notes));
+const writeNotesToFile = async (notes) => await fs.writeFile(notesPath, JSON.stringify(notes))
 
 const removeNote = async (id) => {
-  const notes = await getNotes();
-  const newNotes = notes.filter((note) => note.id != id);
-  await writeNotesToFile(newNotes);
-  console.log(chalk.green('OK. Note was removed'));
-};
+  const notes = await getNotes()
+  const newNotes = notes.filter((note) => note.id != id)
+  await writeNotesToFile(newNotes)
+  console.log(chalk.green('OK. Note was removed'))
+}
+
+const editNote = async (id, title) => {
+  const notes = await getNotes()
+  const note = notes.find((note) => Number(note.id) === Number(id))
+  if (note) {
+    const updatedNotes = notes.map((note) => (Number(note.id) !== Number(id) ? note : { ...note, title }))
+    await writeNotesToFile(updatedNotes)
+    console.log(chalk.green('OK. Note was updated'))
+  } else {
+    console.log('Reject. Note id not found')
+  }
+}
 
 const addNote = async (title) => {
-  const notes = await getNotes();
+  const notes = await getNotes()
   const note = {
     title,
     id: Date.now().toString(),
-  };
-  notes.push(note);
-  await writeNotesToFile(notes);
-  console.log(chalk.green('OK. Note was added'));
-};
+  }
+  notes.push(note)
+  await writeNotesToFile(notes)
+  console.log(chalk.green('OK. Note was added'))
+}
 
 const getNotes = async () => {
-  const notes = await fs.readFile(notesPath, { encoding: 'utf-8' });
-  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
-};
+  const notes = await fs.readFile(notesPath, { encoding: 'utf-8' })
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+}
 
 const printNotes = async () => {
-  const notes = await getNotes();
-  console.log(chalk.blue('Here is the list of notes:'));
-  notes.forEach(({ id, title }) => console.log(`id: ${chalk.red(id)} title: ${chalk.yellow(title)}`));
-};
+  const notes = await getNotes()
+  console.log(chalk.blue('Here is the list of notes:'))
+  notes.forEach(({ id, title }) => console.log(`id: ${chalk.red(id)} title: ${chalk.yellow(title)}`))
+}
 
 module.exports = {
   addNote,
   printNotes,
   removeNote,
-};
+  editNote,
+}
