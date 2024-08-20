d6db96c5ca9a0cb6ac458818a869fab1f647fcaf
AngryPolecat <mvv.tfoms@gmail.com>
2024-08-20 16:38:56 +0700

    done extra task 2 20.08 16:38

--- 04-web-server/public/app.js
+++ 04-web-server/public/app.js
@@ -1,35 +1,59 @@
-console.log('Hello from client script');
+console.log('Hello from client script')
 
-document.addEventListener('click', (event) => {
-  const dataType = event.target.dataset.type;
+document.addEventListener('click', ({ target }) => {
+  const dataType = target.dataset.type
   if (dataType === 'remove') {
-    const id = event.target.dataset.id;
+    const id = target.dataset.id
     removeNote(id).then(() => {
-      event.target.closest('li').remove();
-    });
+      target.closest('li').remove()
+    })
   }
 
   if (dataType === 'edit') {
-    const id = event.target.dataset.id;
-    const text = event.target.dataset.title;
-    const newText = prompt('Редактировать', text);
-    if (newText) {
-      updateNote(id, newText).then((res) => {
-        const containerTitle = event.target.closest('li').querySelector('.container-title');
-        containerTitle.innerHTML = newText;
-      });
+    const id = target.dataset.id
+    const text = target.dataset.title
+    const container = target.closest('li')
+    const initialHTML = container.innerHTML
+
+    container.innerHTML = `
+      <input type="text" value="${text}" />
+      <div>
+        <button class="btn btn-success" data-type="save">Сохранить</button>
+        <button class="btn btn-danger" data-type="cancel">Отменить</button>
+      </div>
+    `
+
+    const updateTitleNote = ({ target }) => {
+      const dataType = target.dataset.type
+      if (dataType === 'save') {
+        const newText = container.querySelector('input').value
+        if (newText) {
+          updateNote(id, newText).then((_res) => {
+            container.innerHTML = initialHTML
+            container.querySelector('.container-title').innerHTML = newText
+            container.querySelector('[data-type="edit"]').dataset.title = newText
+            container.removeEventListener('click', updateTitleNote)
+          })
+        }
+      }
+      if (dataType === 'cancel') {
+        container.removeEventListener('click', updateTitleNote)
+        container.innerHTML = initialHTML
+      }
     }
+
+    container.addEventListener('click', updateTitleNote)
   }
-});
+})
 
 const removeNote = async (id) =>
   await fetch(`/${id}`, {
     method: 'DELETE',
-  });
+  })
 
 const updateNote = async (id, title) =>
   await fetch(`/${id}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json;charset=utf-8' },
     body: JSON.stringify({ title }),
-  });
+  })
